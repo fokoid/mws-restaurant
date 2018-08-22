575108ad204c8585770c440793786e4ed65dcc7a
Alex Thorne <alex@thorne.io>
2018-08-22 23:11:58 +0100

    feat: Move neighbourhood to card header

--- js/main.js
+++ js/main.js
@@ -159,6 +159,11 @@ createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
   li.classList.add('card', 'restaurant');
 
+  const neighborhood = document.createElement('div');
+  neighborhood.innerHTML = restaurant.neighborhood;
+  neighborhood.classList.add('restaurant-neighborhood', 'card-header');
+  li.append(neighborhood);
+
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
@@ -167,11 +172,6 @@ createRestaurantHTML = (restaurant) => {
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
   li.append(name);
-
-  const neighborhood = document.createElement('p');
-  neighborhood.innerHTML = restaurant.neighborhood;
-  neighborhood.classList.add('restaurant-neighborhood');
-  li.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
