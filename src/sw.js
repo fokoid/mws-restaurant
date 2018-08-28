ec56300a155baef86be9213f33edd573606a9e4e
Alex Thorne <alex@thorne.io>
2018-08-28 12:15:15 +0100

    chore: Bump service worker cache version

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 63;
+const CACHE_VERSION = 64;
 const CACHE_PREFIX = 'restaurant-reviews-';
 const CACHE_NAME = `${CACHE_PREFIX}v${CACHE_VERSION}`;
 const IMG_CACHE_NAME = `${CACHE_PREFIX}images`;
